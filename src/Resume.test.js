06168276830de21ef7dffcd3d909b226ca60069b
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2019-09-23 15:05:45 +0200

    Add test file.

---
+++ src/Resume.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Resume from './Resume';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Resume />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
